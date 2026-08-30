09d5a3991394abfdb57fe4febdf7817537332722
tanha-tasri <tanha224517@gmai.com>
2026-08-30 23:43:36 +0600

    update gitignore file

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,5 +5,5 @@ const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
 });
