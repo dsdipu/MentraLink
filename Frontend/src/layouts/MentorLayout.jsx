57738afc42f770b569b0972e7ad51777adb0a44a
dsdipu <mr.sarkar9979@gmail.com>
2026-09-03 11:35:52 +0600

    fixed dashboard color so that logo appear well

--- Frontend/src/layouts/MentorLayout.jsx
+++ Frontend/src/layouts/MentorLayout.jsx
@@ -18,8 +18,8 @@ const MentorLayout = () => {
   return (
     <div className="flex min-h-screen">
       <aside className="w-64 bg-brand-gradient-vertical text-white flex flex-col">
-        <div className="p-4 border-b border-white/20 flex items-center justify-center">
-          <img src={logo} alt="MentraLink" className="h-12 object-contain" />
+        <div className="py-4 bg-white border-b border-gray-200 flex items-center justify-center">
+          <img src={logo} alt="MentraLink" className="h-16 object-contain" />
         </div>
         <nav className="flex-1 p-4 space-y-1">
           {navItems.map((item) => (
